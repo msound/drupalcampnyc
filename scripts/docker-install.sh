49476c8c2287c17f01f2ea829637bb545d9e2826
Mani Soundararajan <mani.sound@gmail.com>
2016-02-27 11:05:27 -0500

    Added docker-install script

---
+++ scripts/docker-install.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+# Usage:
+# docker-install.sh
+#
+
+cd /var/www/html
+
+# Prepare installation (create settings.php, files folder etc)
+drush site-install \
+  --db-url="mysql://${DRUPALDEMO_DB_USER}:${DRUPALDEMO_DB_PASS}@${DRUPALDEMO_DB_HOST}/${DRUPALDEMO_DB_NAME}" \
+  -y synapp install_configure_form.update_status_module='array(FALSE,FALSE)' \
+&& chown -R www-data:www-data sites/default/files
+
