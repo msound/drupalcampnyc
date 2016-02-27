61ab4a3d335394758511186ee780114800f9a700
Mani Soundararajan <mani.sound@gmail.com>
2016-02-27 11:20:34 -0500

    Added drupalcampnyc profile

---
+++ html/profile/drupalcampnyc/drupalcampnyc.profile
@@ -0,0 +1,5 @@
+<?php
+
+/**
+ * Drupal requires this file to be here, even if empty.
+ */
--- scripts/docker-install.sh
+++ scripts/docker-install.sh
@@ -9,6 +9,6 @@ cd /var/www/html
 # Prepare installation (create settings.php, files folder etc)
 drush site-install \
   --db-url="mysql://${DRUPALDEMO_DB_USER}:${DRUPALDEMO_DB_PASS}@${DRUPALDEMO_DB_HOST}/${DRUPALDEMO_DB_NAME}" \
-  -y synapp install_configure_form.update_status_module='array(FALSE,FALSE)' \
+  -y drupalcampnyc install_configure_form.update_status_module='array(FALSE,FALSE)' \
 && chown -R www-data:www-data sites/default/files
 
